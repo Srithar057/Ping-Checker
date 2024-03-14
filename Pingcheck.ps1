6e1cc741446edcd60610825650b30a5d2e434d50
Srithar Parthasarathi <119922855+Srithar057@users.noreply.github.com>
2024-03-14 18:08:09 +0530

    Update Pingcheck.ps1

--- Pingcheck.ps1
+++ Pingcheck.ps1
@@ -3,38 +3,47 @@ $GREEN = [ConsoleColor]::Green
 $GRAY = [ConsoleColor]::Gray
 $MAGENTA = [ConsoleColor]::Magenta
 # Linkedin - https://in.linkedin.com/in/sritharparthasarathi
-function EchoWithColor($color, $text) {
-    $originalColor = $host.UI.RawUI.ForegroundColor
-    $host.UI.RawUI.ForegroundColor = $color
-    Write-Host $text
-    $host.UI.RawUI.ForegroundColor = $originalColor
-}
 
-$ipFile = $args[0]
+$total_ips = (Get-Content $args[0] | Measure-Object -Line).Lines
 
-$ipList = Get-Content $ipFile
-$oc = $ipList.Count
+$successful_ips = @()
+$failed_ips = @()
 
-# Linkedin - https://in.linkedin.com/in/sritharparthasarathi
-$success = 0
-$failed = 0
-Write-Host "--------------------"
-foreach ($ip in $ipList) {
-    if (Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue) {
-        EchoWithColor $GREEN "$ip"
-        $success++
-    } else {
-        EchoWithColor $RED "$ip"
-        $failed++
+# Function to check the reachability of IPs
+function Ping-Check {
+    Write-Host ""
+    # Linkedin - https://in.linkedin.com/in/sritharparthasarathi
+    Write-Host "--------------------"
+    Get-Content $args[0] | ForEach-Object {
+        $ip = $_
+        if (Test-Connection -Count 1 -ComputerName $ip -Quiet) {
+            $successful_ips += $ip
+        }
+        else {
+            $failed_ips += $ip
+        }
     }
-}
+    # Linkedin - https://in.linkedin.com/in/sritharparthasarathi
+    Write-Host "Reached IPs:"
+    $successful_ips | ForEach-Object {
+        Write-Host "$($_)" -ForegroundColor Green
+    }
 
+    Write-Host ""
+    Write-Host "Unreached IPs:"
+    $failed_ips | ForEach-Object {
+        Write-Host "$($_)" -ForegroundColor Red
+    }
+
+    Write-Host "--------------------"
+    Write-Host "Results Count:"
+    Write-Host "Success : $($successful_ips.Count)/$total_ips" -ForegroundColor Green
+    Write-Host "Failed  : $($failed_ips.Count)/$total_ips" -ForegroundColor Red
+    Write-Host "--------------------"
+    Write-Host ""
+    Write-Host "Github: https://github.com/Srithar057" -ForegroundColor Magenta
+    Write-Host ""
+}
 # Linkedin - https://in.linkedin.com/in/sritharparthasarathi
-Write-Host "--------------------"
-Write-Host "Results Count:"
-EchoWithColor $GREEN "Success : $success/$oc"
-EchoWithColor $RED "Failed  : $failed/$oc"
-Write-Host "---------------------"
-Write-Host ""
-EchoWithColor $MAGENTA "Github: https://github.com/Srithar057"
-Write-Host ""
+Ping-Check $args[0]
+# Linkedin - https://in.linkedin.com/in/sritharparthasarathi
